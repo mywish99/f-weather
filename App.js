b050e24396404e3d4979563a4305ae1e69211386
MyWish <mywish99@gmail.com>
2019-04-07 07:20:39 +0900

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! ^^</Text>
+        <Text>Open up App.js!!!</Text>
       </View>
     );
   }
