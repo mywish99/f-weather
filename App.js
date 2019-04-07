a94cd5f405ecf8e4b2ac90bcc64a120bbeb32428
MyWish <mywish99@gmail.com>
2019-04-07 23:12:15 +0900

    final

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image, StatusBar } from "react-native";
+import Weather from "./Weather";
+
+const API_KEY = "c7dd62ae146b5fe2e21f0e824b6bb296";
 
 export default class App extends Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({ error: error });
+      }
+    );
+  }
+
+  _getWeather = (lat, long) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}&units=metric`
+    )
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        });
+      });
+  };
+
   render() {
+    const { isLoaded, error, temperature, name } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js!!! 이제는 오래?? this??</Text>
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather weatherName={name} temp={Math.floor(temperature)} />
+        ) : (
+            <View style={styles.loading}>
+              <Text style={styles.loadingText}>Getting the weather</Text>
+              {error ? <Text style={styles.errorText}>{error}</Text> : null}
+            </View>
+          )}
       </View>
     );
   }
@@ -14,8 +59,21 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 24
+  }
 });
